a1dc678287b5b98dadace233ab6f2e837ba3dd05
Amogh <amoghdatt@gmail.com>
2022-07-09 01:58:14 +0530

    [EKNK] add user seed

---
+++ seeds/user-seed.js
@@ -0,0 +1,15 @@
+module.exports = function UserSeeds() {
+  return [
+    {
+      guid: '183438c9-6e1f-44ac-9c33-7e5a1b408074',
+      firstName: 'Loreum',
+      lastName: 'Ipsum',
+      email: 'lorem.ipsum@example.com',
+      password: '$2a$12$H.nS/XE5VSuzwLG9eJJsLO8yWIEObygrp5t3JBdUGGVw8mG6Asycy',
+      phoneContact: {
+        countryCode: '91',
+        phoneNumber: '1234567890'
+      }
+    }
+  ];
+};
